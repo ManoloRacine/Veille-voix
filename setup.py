603aa0d43ebf7d0cba62e2451ff945faf93c9c56
ManoloRacine <manolo191202@gmail.com>
2022-09-01 20:33:36 -0400

    ajout de setup.py pour les dépendences

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Veille-Voix',
+    version='0.1',
+    packages=['Veille-Voix'],
+    install_requires=['SpeechRecognition'],
+    url='',
+    license='',
+    author='Manolo Racine',
+    author_email='manolo191202@gmail.com',
+    description=''
+)
